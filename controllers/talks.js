4399b78a66f5690f0156548134e658a59a12e877
krabecb <briankrabec@gmail.com>
2024-01-18 10:26:09 -0600

    Push updates

--- controllers/talks.js
+++ controllers/talks.js
@@ -18,7 +18,7 @@ const Talk = require('../models/talk.js')
 async function index(req, res, next) {
     try {
         const talks = await Talk.find({})
-        res.render('talks/index', { title: "What everyone's talking about", talks: talks})
+        res.render('talks/index', { title: "What everyone's talking about.", relative: '', talks: talks})
     } catch(err) {
         console.log(err)
         next(err)
@@ -27,7 +27,7 @@ async function index(req, res, next) {
 
 async function newTalk(req, res, next) {
     try {
-        res.render('talks/new', { title: 'Add Talk', error: ''})
+        res.render('talks/new', { title: 'Add a talk.', relative: '', error: ''})
     } catch(err) {
         console.log(err)
         next(err)
@@ -36,7 +36,7 @@ async function newTalk(req, res, next) {
 
 async function show(req, res, next) {
     try {
-        res.render('talks/show', { title: '', talk: await Talk.findById(req.params.id)})
+        res.render('talks/show', { title: '', relative: true, talk: await Talk.findById(req.params.id)})
     } catch(err) {
         console.log(err)
         next(err)
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TrailTalks' })
+  res.render('index', { title: 'TrailTalks', relative: '', })
 })
 
 router.get('/auth/google', passport.authenticate(
